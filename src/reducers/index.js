e1f7ca253a2a3198dcbe24f07ff236ad00d61cc8
CrioUser <crio-user@criodo.com>
2023-08-04 23:29:31 +0530

    completed milestone 6

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import {preferencesReducer} from "./preferencesReducer"
 import {todoReducer} from "./todoReducer"
-import { combineReducers } from "../library/combineReducers"
+// import { combineReducers } from "../library/combineReducers"
+import { combineReducers } from 'redux';
 
 export const rootReducer = combineReducers({
   "preferencesReducer": preferencesReducer,
