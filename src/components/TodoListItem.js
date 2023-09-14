5b41e451cddfe377144f106b7f7a132620b97ff1
CrioUser <crio-user@criodo.com>
2023-09-14 12:01:46 +0530

    Added toggle todos feature

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import IconCheck from "../images/icon-check.svg";
+import { toggleTodo } from "../slices/todoSlice";
 
 const TodoListItem = ({ todo }) => {
 
+  const dispatch = useDispatch();
   const classes = todo.completed ? "completed" : "";
   const checkIcon = todo.completed ? (
     <img src={IconCheck} alt="Completed" />
   ) : (
     ""
   );
+
+  const handleTodo = () => {
+    dispatch(toggleTodo(todo.id));
+  }
 
   return (
     <li className={classes}>
@@ -19,7 +26,7 @@ const TodoListItem = ({ todo }) => {
         name="completed-checkbox"
         defaultChecked={todo.completed}
       />
-      <div className="checkbox-border-wrap">
+      <div className="checkbox-border-wrap" onClick={handleTodo}>
         <span className="checkbox">
           {checkIcon}
         </span>
