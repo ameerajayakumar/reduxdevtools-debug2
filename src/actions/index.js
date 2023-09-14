0b33805612fd8c1b3ddfebfb4bc7902057af8d26
CrioUser <crio-user@criodo.com>
2023-09-14 10:38:20 +0530

    removed the unnecessary actions folder

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import {TOGGLE_DARK_MODE, CHANGE_FILTER_STATUS} from "./types";
-
-export const toggleDarkMode = () => {
-    return { type: TOGGLE_DARK_MODE }
-}
-  
-export const changeFilterStatus = (id) => {
-    return{ type: CHANGE_FILTER_STATUS, payload: id }
-}
