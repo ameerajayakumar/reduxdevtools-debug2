db3969ead3b3cd7b5783a4d66ebaf3e4f1596861
Ameera Jayakumar <ameeraj.mec@gmail.com>
2024-12-10 16:45:09 +0200

    debugged

--- src/components/TodoFilterControl.js
+++ src/components/TodoFilterControl.js
@@ -1,40 +1,38 @@
-import React from "react";
-import { useState, useEffect } from "react"
-import store from "../todoStore"
+import React from 'react';
+import { useState, useEffect } from 'react';
+import store from '../todoStore';
 // import {changeFilterStatus} from "../actions";
-import { changeFilterStatus } from "../slices/todoSlice";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-
+import { changeFilterStatus } from '../slices/todoSlice';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 let statuses = [
-    { id: "all", title: "All" },
-    { id: "active", title: "Active" },
-    { id: "completed", title: "Completed" }
-]
+  { id: 'all', title: 'All' },
+  { id: 'active', title: 'Active' },
+  { id: 'completed', title: 'Completed' },
+];
 
 const TodoFilterControl = () => {
-    const filterStatus = useSelector(state => state.todoReducer.filterStatus);
+  const filterStatus = useSelector((state) => state.todoReducer.filterStatus);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    return (
-        <div className="control-btn group">
-            {statuses.map((status, idx) => {
-                return (
-                    <button
-                        id={status.id}
-                        className={filterStatus === status.id ? "btn active" : "btn"}
-                        onClick={() => dispatch(changeFilterStatus(status.idd))}
-                        key={idx}
-                    >
-                        {status.title}
-                    </button>
-                )
-            }
-            )}
-        </div>
-    );
+  return (
+    <div className="control-btn group">
+      {statuses.map((status, idx) => {
+        return (
+          <button
+            id={status.id}
+            className={filterStatus === status.id ? 'btn active' : 'btn'}
+            onClick={() => dispatch(changeFilterStatus(status.id))}
+            key={idx}
+          >
+            {status.title}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default TodoFilterControl;
